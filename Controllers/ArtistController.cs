adbd1506da2563fe2b517e94e18118cbeb367402
Tommy Tham <tommytham@hotmail.co.uk>
2023-11-06 21:23:31 +0000

    Add project files.

---
+++ Controllers/ArtistController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicApplication.Controllers
+{
+    public class ArtistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
